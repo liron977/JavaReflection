a5542038d8d405791fd72a2f0625865cf5f03c0c
Liron aviv Kotev <lironkt@mta.ac.il>
2022-07-22 20:07:34 +0300

    func

--- src/reflection/api/InvestigateClass.java
+++ src/reflection/api/InvestigateClass.java
@@ -74,17 +74,47 @@ public class InvestigateClass implements Investigator {
     }
 
     @Override
+    /**
+     * checks if the given class extends a super class, that is not Object (which all extend by default, implicitly)
+     *
+     * @return true if the class extends, false otherwise
+     */
     public boolean isExtending() {
-        return false;
-    }
+      if(clazz.getSuperclass().equals(Object.class)||(clazz.equals(Object.class)))
+      {
+          return false;
+      }
 
+        return true;
+    }
+    /**
+     * get the name of the direct parent class
+     * the name of the class is the short, simple name and not the fully qualified name
+     *
+     * @return the name of the parent class. null if this class doesn't extend other class
+     */
     @Override
     public String getParentClassSimpleName() {
+        if(this.isExtending()) {
+            return clazz.getSuperclass().getSimpleName();
+        }
         return null;
     }
-
+    /**
+     * checks if the given class parent is of type abstract
+     *
+     * @return true if the parent class is abstract, false otherwise (also in case the given class does not extends any other class)
+     */
     @Override
     public boolean isParentClassAbstract() {
+        if(this.isExtending())
+        {
+            return false;
+        }
+        else if(Modifier.isAbstract( clazz.getSuperclass().getModifiers()))
+        {
+            return true;
+        }
         return false;
     }
 
@@ -93,6 +123,17 @@ public class InvestigateClass implements Investigator {
         return null;
     }
 
+    /**
+     * creates a new instance of the given class, using a specific constructor,
+     * to be determined by the number of given arguments.
+     * No worries: there will be no ambiguity, i.e. two constructors that gets in 2 arguments each one of them...
+     * This method should act as a standalone and without any side effects, that is, the newly created
+     * instance SHOULD NOT replace the original given instance.
+     * @param //numberOfArgs number of arguments that a specific constructor has. can be 0.
+     * @param args arguments to pass to the constructor
+     *
+     * @return the newly created instance
+     */
     @Override
     public int invokeMethodThatReturnsInt(String methodName, Object... args) {
         return 0;
